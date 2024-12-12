9c46daafec4adf62c7bcdd6f844691dea49e5c1b
SeoulSKY <contact@seoulsky.dev>
2024-12-12 15:07:38 -0600

    Add descriptions for param and return for docstrings

--- task.py
+++ task.py
@@ -22,11 +22,17 @@ class Task:
     priority: Priority
 
     def __hash__(self) -> int:
-        """Return a hash code for this task."""
+        """Return a hash code for this task.
+
+        :return: Hash code for this task.
+        """
         return hash(self.title)
 
     def __eq__(self, other: object) -> bool:
-        """Check if this task is equal to another object."""
+        """Check if this task is equal to another object.
+
+        :return: True if this task is equal to another object, False otherwise.
+        """
         if not isinstance(other, Task):
             return False
 
@@ -41,17 +47,32 @@ class TaskManager:
         self._tasks: list[set[Task]] = [set() for _ in range(len(Priority))]
 
     def add_task(self, task: Task) -> None:
-        """Add a task. Time complexity: O(1)."""
+        """Add a task.
+        Time complexity: ``O(1)``.
+
+        :param task: Task to add.
+        :raises ValueError: If the task with the same title already exists.
+        """
+        if task in self._tasks[task.priority.value]:
+            raise ValueError(f"Task with the title '{task.title}' already exists.")
+
         self._tasks[task.priority.value].add(task)
 
     def add_tasks(self, tasks: Iterable[Task]) -> None:
-        """Add multiple tasks. Time complexity: O(n) where n is the number of tasks."""
+        """Add multiple tasks.
+        Time complexity: ``O(n)`` where n is the number of tasks.
+
+        :param tasks: Tasks to add.
+        :raises ValueError: If the task with the same title already exists.
+        """
         for task in tasks:
             self.add_task(task)
 
     def get_all_tasks(self) -> Iterable[Task]:
         """Get all tasks sorted by their priority.
-        Time complexity: O(n) where n is the number of tasks.
+        Time complexity: ``O(n)`` where n is the number of tasks.
+
+        :return: All tasks that are sorted by priority from highest to lowest.
         """
         tasks = []
         for priority in Priority:
